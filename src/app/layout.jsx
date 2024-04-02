cfb91b5f721d6189b7b0d3909ddeffb3fdb906b8
Leitz <leitz@mikeleitz.com>
2024-04-02 14:14:27 -0500

    try to add plausible tracking

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,11 +3,13 @@ import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
 
+import PlausibleProvider from 'next-plausible'
+
 export const metadata = {
   title: {
     template: '%s - Leitz',
     default:
-      'Mike Leitz - Software developer, architect, and manager',
+      'Mike Leitz - Staff Software developer, manager',
   },
   description:
     'I\'m Leitz, a software guy who\'s done every job in tech. Based in Nashville, TN. I help people understand the tech world by sharing wisdom acquired in my 25 years of professional delopment experience.',
@@ -21,6 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <head>
+        <PlausibleProvider domain="mikeleitz.com" />
+      </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
