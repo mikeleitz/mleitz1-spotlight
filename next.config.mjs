c21423731bedd12cf105d69b176f66416afeb4d6
Leitz <leitz@mikeleitz.com>
2024-04-04 07:08:42 -0500

    add redirects for broken links.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,22 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/blog/tools/decompile-java-cli/entry.html',
+        destination: '/articles/decompile-java-class-files',
+        permanent: true,
+      },
+      // Wildcard path matching
+      // {
+      //   source: '/blog/:slug',
+      //   destination: '/news/:slug',
+      //   permanent: true,
+      // },
+    ]
+  },
 }
 
 const withMDX = nextMDX({
