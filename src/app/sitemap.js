ed33c37043e124bc3fc971aaac1322894d618cef
Leitz <leitz@mikeleitz.com>
2024-06-13 16:44:57 -0500

    add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,112 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://mikeleitz.com',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://mikeleitz.com/about',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: 'https://mikeleitz.com/creations',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://mikeleitz.com/articles',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/atool-rules-them-all',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/behavioral-annotations',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/christmas',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/database-performance-datatypes',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/decompile-java-class-files',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/do-you-have-a-boomer-manager',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/everything-is-amazing',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/fist-fight',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/kubernetes-basics',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/mcdonalds-8th-wonder-of-the-world',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/price-of-productivity',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/tmux-the-terminal-emulator',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/your-tech-interview-sucks-part-1',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.6,
+        },
+        {
+            url: 'https://mikeleitz.com/articles/your-tech-interview-sucks-part-2',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.6,
+        },
+    ]
+}
